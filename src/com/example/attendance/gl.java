908d4f254e0ce3f21a2f3da22e8c175e73affa79
etenesaca <edgartenesaca@gmail.com>
2013-12-02 18:05:47 -0500

    Version Mejorada

--- src/com/example/attendance/gl.java
+++
@@ -1,6 +0,0 @@
-package com.example.attendance;
-
-public class gl {
-	public static boolean connected;        //Indica si se ha podido realizar la conexion al Servidor
-    public static String login_status;     //Indica si se ha podido realizar la conexion al Servidor
-}
