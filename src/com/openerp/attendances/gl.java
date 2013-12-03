83c364a20c5f8d6b286d941cf515d4826e82fba3
etenesaca <edgartenesaca@gmail.com>
2013-12-03 14:12:43 -0500

    Implementada libreria para conexion al Servidor de Openerp

---
+++ src/com/openerp/attendances/gl.java
@@ -0,0 +1,6 @@
+package com.openerp.attendances;
+
+public class gl {
+	public static boolean connected;        //Indica si se ha podido realizar la conexion al Servidor
+    public static String login_status;     //Indica si se ha podido realizar la conexion al Servidor
+}
