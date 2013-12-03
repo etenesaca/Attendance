c9a417e014427f7a65c37fff2ee9c6f86a7f7e7c
etenesaca <edgartenesaca@gmail.com>
2013-12-03 07:10:51 -0500

    Implementando Configuraciones

--- gen/com/openerp/attendances/R.java
+++ gen/com/openerp/attendances/R.java
@@ -24,18 +24,47 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int btnCancel=0x7f08000c;
+        public static final int btnSave=0x7f08000b;
+        public static final int cmbDb=0x7f080006;
+        public static final int imageView1=0x7f080000;
+        public static final int mnAcercade=0x7f08000e;
+        public static final int mnExit=0x7f08000f;
+        public static final int mnSettings=0x7f08000d;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f080007;
+        public static final int textView5=0x7f080009;
+        public static final int txtPassword=0x7f08000a;
+        public static final int txtPort=0x7f080004;
+        public static final int txtServer=0x7f080002;
+        public static final int txtUsername=0x7f080008;
     }
     public static final class layout {
-        public static final int activity_register_=0x7f030000;
+        public static final int activity_config=0x7f030000;
+        public static final int activity_register_=0x7f030001;
     }
     public static final class menu {
-        public static final int register_=0x7f070000;
+        public static final int menu_register=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_acercade=0x7f05000b;
+        public static final int action_exit=0x7f05000a;
+        /**  Menu 
+         */
+        public static final int action_settings=0x7f050009;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int cancel=0x7f050008;
+        public static final int database=0x7f050004;
+        public static final int hello_world=0x7f050001;
+        public static final int password=0x7f050006;
+        public static final int port=0x7f050003;
+        public static final int save=0x7f050007;
+        /**  Settings 
+         */
+        public static final int server=0x7f050002;
+        public static final int username=0x7f050005;
     }
     public static final class style {
         /** 
