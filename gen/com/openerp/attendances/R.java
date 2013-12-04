09c0e3f6874395c0529b6ba41ead574eff583280
etenesaca <edgartenesaca@gmail.com>
2013-12-04 11:26:51 -0500

    Implementado un perfil de Usuario

--- gen/com/openerp/attendances/R.java
+++ gen/com/openerp/attendances/R.java
@@ -10,6 +10,9 @@ package com.openerp.attendances;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int gray=0x7f070000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -21,35 +24,53 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int configurations=0x7f020000;
-        public static final int connected=0x7f020001;
-        public static final int friends=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int backgroun_user=0x7f020000;
+        public static final int configurations=0x7f020001;
+        public static final int connected=0x7f020002;
+        public static final int friends=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int btnCancel=0x7f08000c;
-        public static final int btnSave=0x7f08000b;
-        public static final int cmbDb=0x7f080006;
-        public static final int imageView1=0x7f080000;
-        public static final int mnAcercade=0x7f08000e;
-        public static final int mnExit=0x7f08000f;
-        public static final int mnSettings=0x7f08000d;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080003;
-        public static final int textView3=0x7f080005;
-        public static final int textView4=0x7f080007;
-        public static final int textView5=0x7f080009;
-        public static final int txtPassword=0x7f08000a;
-        public static final int txtPort=0x7f080004;
-        public static final int txtServer=0x7f080002;
-        public static final int txtUsername=0x7f080008;
+        public static final int btnCancel=0x7f09000c;
+        public static final int btnSave=0x7f09000b;
+        public static final int cmbDb=0x7f090006;
+        public static final int imageView1=0x7f090000;
+        public static final int mnAcercade=0x7f09001e;
+        public static final int mnExit=0x7f09001f;
+        public static final int mnSettings=0x7f09001d;
+        public static final int pctFoto=0x7f09001c;
+        public static final int textView1=0x7f090001;
+        public static final int textView10=0x7f090017;
+        public static final int textView11=0x7f090016;
+        public static final int textView12=0x7f09000e;
+        public static final int textView13=0x7f090014;
+        public static final int textView14=0x7f090013;
+        public static final int textView15=0x7f090010;
+        public static final int textView16=0x7f090012;
+        public static final int textView17=0x7f090011;
+        public static final int textView18=0x7f09000f;
+        public static final int textView19=0x7f09000d;
+        public static final int textView2=0x7f090003;
+        public static final int textView3=0x7f090005;
+        public static final int textView4=0x7f090007;
+        public static final int textView5=0x7f090009;
+        public static final int textView6=0x7f090019;
+        public static final int textView7=0x7f09001a;
+        public static final int textView8=0x7f090018;
+        public static final int textView9=0x7f090015;
+        public static final int txtName=0x7f09001b;
+        public static final int txtPassword=0x7f09000a;
+        public static final int txtPort=0x7f090004;
+        public static final int txtServer=0x7f090002;
+        public static final int txtUsername=0x7f090008;
     }
     public static final class layout {
         public static final int activity_config=0x7f030000;
-        public static final int activity_register_=0x7f030001;
+        public static final int activity_profile=0x7f030001;
+        public static final int activity_register_=0x7f030002;
     }
     public static final class menu {
-        public static final int menu_register=0x7f070000;
+        public static final int menu_register=0x7f080000;
     }
     public static final class string {
         public static final int action_acercade=0x7f05000b;
--- src/com/openerp/attendances/activities/RegisterActivity.java
+++ src/com/openerp/attendances/activities/RegisterActivity.java
@@ -2,19 +2,63 @@ package com.openerp.attendances.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
+import com.openerp.attendances.Configuration;
 import com.openerp.attendances.R;
 
-public class RegisterActivity extends Activity {
+public class RegisterActivity extends Activity implements OnClickListener {
+	private Configuration config;
+	private QuickContactBadge pctFoto;
+	private TextView txtNombre;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register_);
+
+		// Crear una instancia de la Clase de Configuraciones
+		config = new Configuration(this);
+
+		pctFoto = (QuickContactBadge) findViewById(R.id.pctFoto);
+		pctFoto.setOnClickListener(this);
+
+		txtNombre = (TextView) findViewById(R.id.txtName);
+
+		// Cargar el Nombre de Usuario
+		txtNombre.setText(config.getName());
+
+		if (config.getPhoto() != null) {
+			// Cargar la Foto
+			byte[] photo = Base64.decode(config.getPhoto(), Base64.DEFAULT);
+			Bitmap bmp = BitmapFactory.decodeByteArray(photo, 0, photo.length);
+			pctFoto.setImageBitmap(bmp);
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.pctFoto:
+			// Ir al perfil del Usuario
+			Intent profile_act = new Intent(this, ProfileActivity.class);
+			startActivity(profile_act);
+			break;
+
+		default:
+			break;
+		}
+
 	}
 
 	@Override
@@ -34,8 +78,8 @@ public class RegisterActivity extends Activity {
 			break;
 		case R.id.mnSettings:
 			// Para ir a la ventana se Configuraciones
-			Intent ventana_menu = new Intent(RegisterActivity.this, ConfigActivity.class);
-			startActivity(ventana_menu);
+			Intent intent_act = new Intent(RegisterActivity.this, ConfigActivity.class);
+			startActivity(intent_act);
 			break;
 
 		default:
