b56b6a8f2d607a539d0671270938b73018858ac1
etenesaca <edgartenesaca@gmail.com>
2013-12-10 23:28:47 -0500

    Agregados un layout para personalizar el listview

---
+++ src/com/openerp/attendances/activities/ItemAdapter.java
@@ -0,0 +1,56 @@
+package com.openerp.attendances.activities;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.openerp.attendances.R;
+
+public class ItemAdapter extends BaseAdapter {
+
+	private Context context;
+	private List<Item> items;
+
+	public ItemAdapter(Context context, List<Item> items) {
+		this.context = context;
+		this.items = items;
+	}
+
+	@Override
+	public int getCount() {
+		return this.items.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return this.items.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		// Create a new view into the list.
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View rowView = inflater.inflate(R.layout.list_item_attendance, parent, false);
+
+		// Set data into the view.
+		ImageView ivItem = (ImageView) rowView.findViewById(R.id.ivItem);
+		TextView tvTitle = (TextView) rowView.findViewById(R.id.tvTitle);
+		tvTitle.setText(this.items.get(position).getTitle());
+		ivItem.setImageResource(this.items.get(position).getImage());
+
+		return rowView;
+	}
+
+}
