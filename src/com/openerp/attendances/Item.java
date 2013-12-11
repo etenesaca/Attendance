d719d8e499da9ce184459439672d5b14a3c27320
etenesaca <edgartenesaca@gmail.com>
2013-12-11 16:12:39 -0500

    - Agregadas una clase de metodos (hupernikao)

---
+++ src/com/openerp/attendances/Item.java
@@ -0,0 +1,33 @@
+package com.openerp.attendances;
+
+public class Item {
+	private Integer image;
+	private String title;
+
+	public Item() {
+		super();
+	}
+
+	public Item(Integer image, String title) {
+		super();
+		this.image = image;
+		this.title = title;
+	}
+
+	public Integer getImage() {
+		return image;
+	}
+
+	public void setImage(Integer image) {
+		this.image = image;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
